be464a7b465c151ec58bb42f4c3949c70eb66744
P J Borowiecki <piotr.borowiecki@yahoo.com>
2024-03-19 01:30:44 +0100

    Implement a basic createCashCard POST endpoint to satisfy the test

--- src/main/java/com/pjborowiecki/cashcards/CashCardController.java
+++ src/main/java/com/pjborowiecki/cashcards/CashCardController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
@@ -25,6 +26,11 @@ public class CashCardController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard() {
+        return null;
     }
 
 }
